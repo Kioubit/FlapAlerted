393131975a940e21a31c367f0fcb406523f943f2
Kioubit <kioubit@localhost.localdomain>
2025-05-27 18:07:52 +0300

    Optimize DeleteLeastValuable and add decay

--- monitor/PathTracker.go
+++ monitor/PathTracker.go
@@ -11,13 +11,15 @@ type PathTracker struct {
 }
 
 type pathEntry struct {
-	key  string
-	info *PathInfo
+	key   string
+	info  *PathInfo
+	ticks int
 }
 
 func (pt *PathTracker) Update(key string) {
 	if elem, exists := pt.paths[key]; exists {
 		pt.order.MoveToBack(elem)
+		elem.Value.(*pathEntry).ticks = 0
 	}
 }
 
@@ -25,6 +27,7 @@ func (pt *PathTracker) Set(key string, info *PathInfo) {
 	if elem, exists := pt.paths[key]; exists {
 		pt.order.MoveToBack(elem)
 		elem.Value.(*pathEntry).info = info
+		elem.Value.(*pathEntry).ticks = 0
 		return
 	}
 
@@ -52,17 +55,23 @@ func (pt *PathTracker) DeleteLeastValuable() {
 	var toDelete = pt.order.Front()
 	var minCount uint64 = math.MaxUint64
 
-	steps := 0
-	for elem := pt.order.Front(); elem != nil && steps < 50; elem = elem.Next() {
-		entry := elem.Value.(*pathEntry)
-		entryCount := entry.info.Count
+	// Oldest element
+	toDelete.Value.(*pathEntry).ticks++
+	if toDelete.Value.(*pathEntry).ticks < 10000 {
+		steps := 0
+		for elem := pt.order.Front(); elem != nil && steps < 50; elem = elem.Next() {
+			entry := elem.Value.(*pathEntry)
+			entryCount := entry.info.Count
 
-		if entryCount < minCount {
-			minCount = entryCount
-			toDelete = elem
+			if entryCount < minCount {
+				minCount = entryCount
+				toDelete = elem
+				if entryCount == 1 {
+					break
+				}
+			}
+			steps++
 		}
-
-		steps++
 	}
 
 	entry := pt.order.Remove(toDelete).(*pathEntry)
